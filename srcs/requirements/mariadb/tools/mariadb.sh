fd0d108dcccb8354491629d6b24ee7c41a0515db
moop250 <hugo.libine@proton.me>
2025-02-20 16:28:48 +0100

    if this dosent work i think ill cry

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,40 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-# Initialize MariaDB data directory
-mysql_install_db
+service mysql start 
 
-# Start MariaDB service
-/etc/init.d/mysql start
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_DATABASE' ;" >> db.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db.sql
+echo "FLUSH PRIVILEGES;" >> db.sql
 
-# Check if the database already exists
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
-    echo "Database already exists"
-else
-    # Secure MariaDB installation using environment variables
-    mysql_secure_installation << EOF
+mysql < db.sql
 
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-n
-Y
-Y
-EOF
+kill $(cat /var/run/mysqld/mysqld.pid)
 
-    # Grant remote access to root user
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
-
-    # Create database and user for WordPress
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
-
-    # Import SQL file into the database
-    mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
-fi
-
-# Stop MariaDB service to allow the container to start it in the foreground
-/etc/init.d/mysql stop
-
-# Execute any command passed to the script
-exec "$@"
-
+mysqld
