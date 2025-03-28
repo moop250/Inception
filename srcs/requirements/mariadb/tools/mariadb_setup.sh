a39fbb22fb1acf46ce2102f57013a9982cd1f077
moop250 <hugo.libine@proton.me>
2025-03-28 18:16:47 +0100

    a couple of last minute changes and the database no longer boot loops!

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -10,17 +10,17 @@ until mysqladmin ping --silent; do
 done
 
 # Check if the database already exists
-if mysql -u root -e "USE $SQL_DATABASE" 2>/dev/null; then
+if mysql -u root -p"$SQL_ROOT_PASSWORD" -e "USE $SQL_DATABASE" 2>/dev/null; then
   echo "Database $SQL_DATABASE already exists. Skipping setup."
 else
   # Create and setup database
   mysql -u root <<EOF
-  CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;
-  CREATE USER IF NOT EXISTS \`$SQL_USER\`@'%' IDENTIFIED BY '$SQL_PASSWORD';
-  GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO \`$SQL_USER\`@'%';
-  ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
-  FLUSH PRIVILEGES;
-  EOF
+CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;
+CREATE USER IF NOT EXISTS \`$SQL_USER\`@'%' IDENTIFIED BY '$SQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO \`$SQL_USER\`@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
 fi
 
 # Restart MariaDB service
