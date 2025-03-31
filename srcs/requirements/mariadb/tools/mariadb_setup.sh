4267c8cc41227bafa588f7c1ec2a2c27b3406f50
moop250 <hugo.libine@proton.me>
2025-03-31 15:00:52 +0200

    that took way too long haha

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -23,6 +23,6 @@ FLUSH PRIVILEGES;
 EOF
 fi
 
-# Restart MariaDB service
+# Restart MariaDB service cleanly
 mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
 exec mysqld_safe
---
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# let mdb_init.sh finish
+sleep 5
+
+# autofill wp startup fields
+wp core download --path="/var/www/html" --allow-root
+wp config create --path="/var/www/html" --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$DB_HOST --skip-check
+wp core install --path="/var/www/html" --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+wp user create --path="/var/www/html" --allow-root $WP_USER $WP_EMAIL --user_pass=$WP_PASS --role='contributor'
+
+# launch php-fpm
+exec /usr/sbin/php-fpm8.2 -F
