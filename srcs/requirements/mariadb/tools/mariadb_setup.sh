59f65be183a38768dfe00842a38b45678999ef78
moop250 <hugo.libine@proton.me>
2025-04-28 20:24:27 +0200

    moving mariadb over to alpine
    because i hate myself :sunglasses:

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,20 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 # Start MariaDB service
-mysqld_safe &
+mariadb-install-db --user=mysql --datadir=/var/lib/mysql --bind_address=0.0.0.0 --port=3306 --tmpdir=/tmp > /dev/null 2> /dev/null
+mariadbd --user=mysql &
+MARIADB_PID=$!
 
 # Wait for the service to be ready
-until mysqladmin ping --silent; do
-  echo "Waiting for MariaDB to start..."
-  sleep 1
+while ! mariadb -u root -h localhost -p"$SQL_ROOT_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1; do
+	echo "Waiting for MariaDB to start..."
+	sleep 1
 done
 
 # Check if the database already exists
-if mysql -u root -p"$SQL_ROOT_PASSWORD" -e "USE $SQL_DATABASE" 2>/dev/null; then
+if mariadb -u root -h localhost -p"$SQL_ROOT_PASSWORD" -e "USE $SQL_DATABASE" 2>/dev/null; then
   echo "Database $SQL_DATABASE already exists. Skipping setup."
 else
   # Create and setup database
-  mysql -u root <<EOF
+  mariadb -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;
 CREATE USER IF NOT EXISTS \`$SQL_USER\`@'%' IDENTIFIED BY '$SQL_PASSWORD';
 GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO \`$SQL_USER\`@'%';
@@ -23,6 +25,6 @@ FLUSH PRIVILEGES;
 EOF
 fi
 
-# Restart MariaDB service cleanly
-mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
-exec mysqld_safe
+# Wait for the mariadb to finish
+echo "MariaDB is running"
+wait "$MARIADB_PID"
