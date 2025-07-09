2783d5e2696a9bb8fd3165efdf47c86c4b77a784
moop250 <hugo.libine@proton.me>
2025-07-09 16:45:21 +0200

    make the static page have its own decicated url

--- srcs/requirements/bonus/static_page/tools/init.sh
+++ srcs/requirements/bonus/static_page/tools/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-mv landing.html /var/www/html/static-page.html
+mkdir /var/www/html/static-page
+mv landing.html /var/www/html/index.html
 mkdir /var/www/html/styles
 mv style.css /var/www/html/styles
